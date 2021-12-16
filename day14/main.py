cd65cad5334cc6860503b2ec5b2fef111d70ad4a
Christian Cavuti <c.cavuti@dataone.it>
2021-12-16 17:41:09 +0100

    Solution for day 14

--- day14/main.py
+++ day14/main.py
@@ -1,5 +1,8 @@
+from collections import defaultdict
+import copy
+
 def get_data():
-    with open("C:/Users/c.cavuti/Desktop/advent-of-code-2021/day14/input2.txt", "r") as file:
+    with open("C:/Users/c.cavuti/Desktop/advent-of-code-2021/day14/input.txt", "r") as file:
         lines = [l.strip() for l in file.readlines()]
         com_dict = dict()
         for command in lines[2:]:
@@ -9,7 +12,7 @@ def get_data():
 
 def quiz1():
     line, commands = get_data()
-    for _ in range(3):
+    for _ in range(10):
         new_line = ""
         for i in range(len(line)-1):
             part = line[i:i+2]
@@ -26,34 +29,41 @@ def quiz1():
     min_count = 10000000000000
     for letter in letters:
         curr_count = line.count(letter)
+        letters_count[letter] = curr_count
         max_count = max(max_count, curr_count)
         min_count = min(min_count, curr_count)
-    
+
     return max_count - min_count
 
 def quiz2():
     line, commands = get_data()
     occurrences = commands.copy()
-    for k in occurrences.keys():
-        occurrences[k] = 0 
+    occurrences = defaultdict(lambda: 0)
     for i in range(len(line)-1):
-        occurrences[line[i]+line[i+1]] = 1
+        occurrences[line[i]+line[i+1]] += 1
 
-    for i in range(3):
-        occ_copy = occurrences.copy()
-        for k, v in occurrences.items():
+    letters = defaultdict(lambda: 0)
+    for letter in line:
+        letters[letter] += 1
+
+    for i in range(40):
+        occ_copy = defaultdict(lambda: 0)
+        for k in sorted(occurrences):
+            v = occurrences[k]
             if v > 0:
-                occ_copy[k] = 0
                 new_first = k[0]+commands[k]
                 new_second = commands[k] + k[1]
+
                 occ_copy[new_first] += v
                 occ_copy[new_second] += v
-            #print(occurrences)
-        occurrences = occ_copy
+                
+                letters[commands[k]] += v
 
+        occurrences = copy.deepcopy(occ_copy)
+    
+    return max(letters.values()) - min(letters.values())
 
-    print(occurrences)
 
 if __name__ == '__main__':
     print(quiz1())
-    quiz2()
+    print(quiz2())
