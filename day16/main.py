4985176c130c7c6b527bd05524c69b72de82b66b
Kavuti <christian.cavuti@gmail.com>
2021-12-24 18:48:56 +0100

    Solution for day 16 part 1

--- day16/main.py
+++ day16/main.py
@@ -1,63 +1,35 @@
 def get_data():
-    with open("/home/christian/Scrivania/advent-of-code-2021/day16/input2.txt", "r") as file:
-        stringed = file.read()
+    with open("input.txt", "r") as file:
+        stringed = file.readlines()[0].strip()
         binary = ""
         for char in stringed:
             binary += (bin(int(char, 16))[2:]).zfill(4)
         return binary
 
-def get_packet_type(packet):
-    return 'literal' if packet[3:6] == '100' else 'operator'
-
-def get_packet_version(packet):
-    return int(packet[0:3], 2)
-
-def get_length_type(packet):
-    return int(packet[6])
-
-def get_total_length(packet):
-    return int(packet[7:22], 2)
-
-def extract_packet(packet):
-    if get_packet_type(packet) == 'literal':
-        curr_char = 6
-        while packet[curr_char] == '1':
-            curr_char += 5
-        return packet[:curr_char+5]
-    else:
-        if get_length_type(packet) == 0:
-            return packet[:23+get_total_length(packet)]
-        else:
-            return packet[:8+(len(packet[7:]) - (len(packet[7:])%11))]
-
-def extract_subpackets(subpackets):
-    packets = []
-    while len(subpackets) >= 11:
-        packet = extract_packet(subpackets)
-        packets.append(packet)
-        subpackets = subpackets[len(packet):]
-    return packets
-
-def get_packet_versions_sum(packet):
-    if get_packet_type(packet) == 'literal':
-        return get_packet_version(packet)
-    else:
-        if get_length_type(packet) == 0:
-            total_length = get_total_length(packet)
-            subpackets = extract_subpackets(packet[22:])
-            return get_packet_version(packet) + sum([get_packet_versions_sum(p) for p in subpackets])
-        else:
-            subpackets = extract_subpackets(packet[7:])
-            return get_packet_version(packet) + sum([get_packet_versions_sum(p) for p in subpackets])
-
-            
-def test():
-    return extract_packet('1101000101001010010001001000000000')
-
 def quiz1():
-    print(get_data())
-    return get_packet_versions_sum(get_data())
-
+    data = get_data()
+    i = 0
+    total = 0
+    while i < len(data) and '1' in data[i:]:
+        version = int(data[i:i+3], 2)
+        total += version
+        ptype = int(data[i+3:i+6], 2)
+        i += 6
+        if ptype == 4:
+            curr_char = i
+            while data[curr_char] == '1':
+                curr_char += 5
+            curr_char += 5
+            i = curr_char
+        else:
+            lt = int(data[i])
+            i += 1
+            if lt == 0:
+                i += 15
+            else:
+                i += 11
+    return total          
+    
 if __name__ == '__main__':
+    print(get_data())
     print(quiz1())
-    print(test())
